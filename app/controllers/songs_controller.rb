25f29f17331ebe5c07198701031e94d042fd16f8
noglows <jessica.noglows@outlook.com>
2015-11-22 17:35:55 -0500

    Added ability to view songs and add a new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,20 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
+
+  def new
+    @song = Song.new
+    @users = User.all
+  end
+
+  def create
+    Song.create(song_params[:song])
+    redirect_to "/songs"
+  end
+
+  private
+
+  def song_params
+    params.permit(song:[:name, :artist, :comments, :user_id])
+  end
 end
