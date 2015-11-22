cf9081e18fa3ffd12dc640de2bbe3c8e5e7c86ef
noglows <jessica.noglows@outlook.com>
2015-11-22 17:51:15 -0500

    Added the ability to vote for a song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,7 +9,23 @@ class SongsController < ApplicationController
   end
 
   def create
-    Song.create(song_params[:song])
+    new_params = song_params[:song]
+    new_params[:votes] = 0
+    Song.create(new_params)
+    redirect_to "/songs"
+  end
+
+  def destroy
+    id = params[:id]
+    Song.destroy(id)
+    redirect_to "/songs"
+  end
+
+  def update
+    id = params[:id]
+    song = Song.find(id)
+    num_votes = song.votes + 1
+    song.update(votes: num_votes)
     redirect_to "/songs"
   end
 
