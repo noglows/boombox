037d77e78da57772deab0df3d5a559108b5cabb4
noglows <jessica.noglows@outlook.com>
2015-11-23 09:52:51 -0500

    Working on sign-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   def signin
     @users = User.all
+    @user = User.new
+  end
+
+  def signed_in
+    #binding.pry
+    new_params = user_params[:user]
+    session[:user_id] = new_params[:id]
+    redirect_to "/songs"
+  end
+
+
+  private
+
+  def user_params
+    params.permit(user:[:id, :name, :fav_genres])
   end
 end
