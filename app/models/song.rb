4e47c9cafa62f2fd65ce1edc054ffae09f890a58
noglows <jessica.noglows@outlook.com>
2015-11-24 11:20:32 -0500

    Prevent entry of blank songs and users

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
+#attr_accessor :name, :artist
+
   belongs_to :user
+
+  # validates :name, presence: true
+  # validates :name, length: { minimum: 3}
+  # validates :artist, presence: true
+  # validates :artist, length: { minimum: 3 }
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+
+
   has_many :songs
+
+  # validates :name, presence: true
+  # validates :name, length: { minimum: 3}
 end
